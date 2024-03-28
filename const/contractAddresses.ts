723e174ba1bf6916a3e993116f34a4fc5a590edd
Yucel <yucelisbrn@gmail.com>
2024-03-28 18:18:59 +0100

    last change

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -2,13 +2,9 @@
 
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+/*import { Mumbai } from "@thirdweb-dev/chains";
 export const NETWORK = Mumbai;
 
-const sdk = new ThirdwebSDK(Mumbai, {
-  apiKey: "e8a3624a0736b9065e49784315f573b0"
-});
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
@@ -21,3 +17,18 @@ export const NFT_COLLECTION_ADDRESS =
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
 export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+*/
+
+import { Mumbai } from "@thirdweb-dev/chains";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+export const NETWORK = Mumbai;
+
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK("Mumbai", {
+  clientId: "e8a3624a0736b9065e49784315f573b0",
+});
+// --- OR ---
+// If used on the BACKEND pass your 'secretKey'
+
+
+const contract = await sdk.getContract("0x32E580f9A872EaEE52625bf8911FEb6B0AcF1352");
